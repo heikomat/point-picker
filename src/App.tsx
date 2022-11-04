ca4b8ef50aa74d8fafce00a2c66cba56a47edf5b
heikomat <heiko.mathes@gmail.com>
2022-11-04 20:48:30 +0100

    bootstrap chakra-app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, VStack } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <VStack>
+      <Box>
+        {'point-picker'}
+      </Box>
+    </VStack>
+  );
+}
+
+export default App;
