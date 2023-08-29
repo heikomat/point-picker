cc017424cc6516e072fcd4612f8c700211685a3a
heikomat <heiko.mathes@gmail.com>
2023-08-29 23:40:37 +0200

    remove unused vars

--- src/components/player-overview/overview-player.tsx
+++ src/components/player-overview/overview-player.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { memo, ReactElement, useCallback } from "react";
 import { Player } from "../../contracts";
 import { PlayerPicture } from "../player-picture";
