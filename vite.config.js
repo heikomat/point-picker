8f72c267f47f1c2ca333fa597aca3477f3c6d361
heikomat <heiko.mathes@tuta.io>
2024-06-17 18:29:24 +0200

    switch to react 19

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,11 @@ const { defineConfig } = require('vite')
 const react = require('@vitejs/plugin-react')
 
 module.exports = defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        ["babel-plugin-react-compiler"],
+      ],
+    },
+  })],
 })
