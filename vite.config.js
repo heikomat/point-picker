a898b98db24c3a2403f2ca5addfd0a9f7366e09c
heikomat <heiko.mathes@tuta.io>
2024-06-17 22:22:34 +0200

    try fixing service-worker-update

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,10 @@ module.exports = defineConfig({
       ],
     },
   })],
+  define: {
+    "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
+    "process.env.PUBLIC_URL": process.env.NODE_ENV === 'production'
+      ? '"https://point-picker.basketbears.com"'
+      : '"http://localhost:5173"'
+  }
 })
