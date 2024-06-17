b243fa459f61fe7a9ccfe0803c2ce2507b11b288
heikomat <heiko.mathes@tuta.io>
2024-06-17 18:21:25 +0200

    switch to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 import { ChakraProvider } from '@chakra-ui/react'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { ToastContainer } from 'react-toastify';
