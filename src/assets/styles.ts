102e9fdad055b8be48370df60085bfbae7cc5915
heikomat <heiko.mathes@tuta.io>
2024-07-21 14:37:21 +0200

    make player blocks red to test auto update

--- src/assets/styles.ts
+++ src/assets/styles.ts
@@ -1,7 +1,7 @@
 import { pxToRem } from "../scale";
 
 export const playerBlock = {
-  backgroundColor: 'gray.100',
+  backgroundColor: 'red.200',
   boxShadow: `1px 1px ${pxToRem(3)}rem 0px #c0c0c0`,
   borderRadius: `${pxToRem(5)}rem`,
 }
