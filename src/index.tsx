977c765f4185ae5cba9d895e4932527e8a4ea4a7
heikomat <heiko.mathes@tuta.io>
2024-07-21 13:57:10 +0200

    update toast messages

--- src/index.tsx
+++ src/index.tsx
@@ -4,37 +4,27 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
+import { registerSW } from 'virtual:pwa-register';
 
 import 'react-toastify/dist/ReactToastify.css';
 
-import { registerSW } from 'virtual:pwa-register';
-
 registerSW({
   onNeedRefresh() {
-    toast.info('New content is available; please refresh.', {
-      position: 'bottom-center',
+    toast.info('Update verfügbar! Neu laden um zu aktualisieren.', {
       autoClose: false,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
       toastId: 'appUpdateAvailable',
+      onClose: () => {
+        window.location.reload();
+      }
     });
   },
   onOfflineReady() {
-    toast.info('Content is cached for offline use.', {
+    toast.info('App-Daten gespeichert. Sie ist jetzt offline nutzbar.', {
       position: 'bottom-center',
       autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
     });
   },
 });
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
